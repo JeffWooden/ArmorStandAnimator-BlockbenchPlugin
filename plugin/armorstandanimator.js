60fed72f3da52fc6da238f571a07de2cdbe415f9
JeffWooden <jeffwooden.courriel@gmail.com>
2021-08-22 16:35:34 +0200

    Added warnUser function + warn time message config option

--- plugin/armorstandanimator.js
+++ plugin/armorstandanimator.js
@@ -1,7 +1,15 @@
+function warnUser(msg,time){
+    return Blockbench.showQuickMessage(msg,time)
+}
+
 (function(){
     // Credit to https://github.com/misode/ (cf. https://github.com/misode/vscode-nbt/blob/master/src/common/Snbt.ts)
     function stringify(t,e,n=""){const r=n+" ";switch(t){case"compound":return 0===Object.keys(e).length?"{}":`{\n${Object.entries(e).map(([t,e])=>`${r}"${t}": ${stringify(e.type,e.value,r)}`).join(",\n")}\n${n}}`;case"list":return 0===e.value.length?"[]":isCompact(e.type)?`[${stringifyEntries(e.type,e.value,"",", ")}]`:`[\n${stringifyEntries(e.type,e.value,r,",\n")}\n${n}]`;case"byteArray":return`[B;${stringifyEntries("byte",e," ",",")}]`;case"intArray":return`[I;${stringifyEntries("int",e," ",",")}]`;case"longArray":return`[L;${stringifyEntries("long",e," ",",")}]`;case"floatArray":return`[${stringifyEntries("float",e,",")}]`;case"string":return'"'+e.replace(/(\\|")/g,"\\$1")+'"';case"byte":return e+"b";case"double":return e+"d";case"float":return e+"f";case"short":return e+"s";case"int":return""+e;case"long":return stringifyLong(e)+"L";default:return"null"}}function stringifyLong(t){return dataView.setInt32(0,Number(t[0])),dataView.setInt32(4,Number(t[1])),""+dataView.getBigInt64(0)}function stringifyEntries(t,e,n,r){return e.map(e=>`${n}${stringify(t,e,n)}`).join(r)}function isCompact(t){return"byte"===t||"double"===t||"float"===t||"short"===t||"int"===t||"long"===t}bytes=new Uint8Array(8),dataView=new DataView(bytes.buffer);
     
+    var config = {
+        warn : 4000
+    }
+
     var menu;
     var load_as_button;
     var export_button;
@@ -22,12 +30,12 @@
             }});
             export_button = new Action("export",{name:"Export Animation",description:"",icon:"engineering",click:function()
             {
-                if(Mode.selected.id != "animate") return Blockbench.showQuickMessage(`You've to open the "Animate" mode before exporting the animation.`, 4000)
+                if(Mode.selected.id != "animate") return warnUser(`You've to open the "Animate" mode before exporting the animation.`, config.warn)
 
                 selectedAnimation = Animator.selected
-                if(selectedAnimation == null) return Blockbench.showQuickMessage(`Please, select an animation before.`, 4000)
+                if(selectedAnimation == null) return warnUser(`Please, select an animation before.`, config.warn)
 
-                if(selectedAnimation.snapping != 20) return Blockbench.showQuickMessage(`Invalid snapping number ! Please, set the animation snapping to "20"`, 4000)
+                if(selectedAnimation.snapping != 20) return warnUser(`Invalid snapping number ! Please, set the animation snapping to "20"`, config.warn)
             }});
 
             // Create menu bar and integrates button in it
