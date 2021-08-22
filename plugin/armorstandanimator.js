030e3106ecb96aa69b603258df37a8836cf9b14a
JeffWooden <jeffwooden.courriel@gmail.com>
2021-08-22 14:33:34 +0200

    Added "Load Armorstand" button

--- plugin/armorstandanimator.js
+++ plugin/armorstandanimator.js
@@ -3,6 +3,7 @@
     function stringify(t,e,n=""){const r=n+" ";switch(t){case"compound":return 0===Object.keys(e).length?"{}":`{\n${Object.entries(e).map(([t,e])=>`${r}"${t}": ${stringify(e.type,e.value,r)}`).join(",\n")}\n${n}}`;case"list":return 0===e.value.length?"[]":isCompact(e.type)?`[${stringifyEntries(e.type,e.value,"",", ")}]`:`[\n${stringifyEntries(e.type,e.value,r,",\n")}\n${n}]`;case"byteArray":return`[B;${stringifyEntries("byte",e," ",",")}]`;case"intArray":return`[I;${stringifyEntries("int",e," ",",")}]`;case"longArray":return`[L;${stringifyEntries("long",e," ",",")}]`;case"floatArray":return`[${stringifyEntries("float",e,",")}]`;case"string":return'"'+e.replace(/(\\|")/g,"\\$1")+'"';case"byte":return e+"b";case"double":return e+"d";case"float":return e+"f";case"short":return e+"s";case"int":return""+e;case"long":return stringifyLong(e)+"L";default:return"null"}}function stringifyLong(t){return dataView.setInt32(0,Number(t[0])),dataView.setInt32(4,Number(t[1])),""+dataView.getBigInt64(0)}function stringifyEntries(t,e,n,r){return e.map(e=>`${n}${stringify(t,e,n)}`).join(r)}function isCompact(t){return"byte"===t||"double"===t||"float"===t||"short"===t||"int"===t||"long"===t}bytes=new Uint8Array(8),dataView=new DataView(bytes.buffer);
     
     var menu;
+    var load_as_button;
     
     Plugin.register('armorstandanimator', {
         title: "Armor Stand Animator",
@@ -14,6 +15,11 @@
         onload(){
             menu = new BarMenu("asa")
             menu.label.innerText = "Armorstand Animator"
+
+            load_as_button = new Action("load_as",{name:"Load Armorstand Model",description:"",icon:"add_circle",click:function()
+            {
+                console.log("Hello World")
+            }});
         },
         onunload(){
             // menu.delete()
